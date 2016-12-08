fc121831fa21425fc88cde3297890c95cf49c79d
Stevie <yzm629@gmail.com>
2016-12-08 11:08:16 +0800

    add store

--- src/store/modules/Location.js
+++ src/store/modules/Location.js
@@ -10,7 +10,7 @@ export default {
         add (state, payload) {
 
             if (typeof payload == 'object') {
-                const is_same = state.current && !Object.is(state.current.position, payload.position);
+                const is_same = state.current && state.current.address.formattedAddress != payload.address.formattedAddress;
                 if(is_same || !state.current){
                     state.current = payload;
                     state.history.push(payload);
